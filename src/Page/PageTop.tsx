46184dab965eaa9d3e7e42439593677300a2c577
Louis Magnier <louis.magnier@utt.fr>
2022-05-10 08:52:47 +0200

    création du composant de preview et début d'intégration dans les pages

--- src/Page/PageTop.tsx
+++ src/Page/PageTop.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Jumbotron} from '../composant/jumbotron'
+import {Preview} from '../composant/preview'
 
 export class PageTop extends React.Component {
     render() {
         return (
             <div className="container-fluid" id="accpage">
-                
+                <Preview titre="Titre du film" realisateur="Luc Besson" affiche=""/>
             </div>
         );
     }
---
+++ src/composant/preview.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type AppProps = {titre : string
+        realisateur : string
+        affiche : string
+    }
+type AppState = {}
+export class Preview extends React.Component <AppProps, AppState> {
+    
+    constructor(props: AppProps){
+        super(props);
+    }
+    
+    render() {
+        return(
+            <div className="row">
+                <div className="col-sm-6 col-md-4">
+                    <div className="thumbnail">
+                    <img src="..."/>
+                    <div className="caption">
+                        <h3>Titre : {this.props.titre}</h3>
+                        <p>Realisateur : {this.props.realisateur} </p>
+                        <p>
+                            <a href="#" className="btn btn-primary" role="button">Voir plus</a> 
+                        </p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
