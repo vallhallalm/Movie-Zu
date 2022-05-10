9377fad1e3428c6b37ba028bf281ac1fb34ca560
Louis Magnier <louis.magnier@utt.fr>
2022-05-10 08:15:51 +0200

    création des composants PageSortie et PageTop

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import './App.css';
 import './bootstrap-3.3.7-dist/css/bootstrap.css';
 import {Navbar} from './composant/navbar';
 import {Footer} from './composant/footer';
-import {Jumbotron} from './composant/jumbotron';
-import {PageAccueil} from './Page/PageAccueil'
+import {PageAccueil} from './Page/PageAccueil';
+import {PageTop} from './Page/PageTop';
+import {PageSortie} from './Page/PageSortie';
 
 function App() {
   return (
     <div className="App">
-      
         <BrowserRouter>
           <Navbar/>
           <Routes>
             <Route  path="/" element={<PageAccueil/>}></Route>
-            <Route path="/Top"></Route>
-            <Route path="/Sortie"></Route>
+            <Route path="/Top" element={<PageTop/>}></Route>
+            <Route path="/Sortie" element={<PageSortie/>}></Route>
           </Routes>
         </BrowserRouter>
-        
-      
       <Footer/>
     </div>
   );
---
+++ src/Page/PageTop.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Jumbotron} from '../composant/jumbotron'
+
+export class PageTop extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid" id="accpage">
+                
+            </div>
+        );
+    }
+}
