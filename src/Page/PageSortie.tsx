32db063dc4ac2c23c6f648a6b91049836539880d
Louis Magnier <louis.magnier@utt.fr>
2022-05-23 07:37:34 +0200

    méthode de requête API fonctionnelle et affichage fonctionnel

--- src/Page/PageSortie.tsx
+++ src/Page/PageSortie.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Preview} from '../composant/preview';
 import {api} from '../api';
+import { title } from 'process';
 
 
 type AppProps = {}
@@ -11,62 +12,48 @@ export class PageSortie extends React.Component <AppProps, AppState> {
     
     constructor(props: AppProps){
         super(props);
-        this.state={donnée:null}
+        this.state={donnée:null};
+        this.componentDidMount=this.componentDidMount.bind(this);
     }
 
-    componentDidUpdate(){
-        if (this.state.donnée===null){
-            let Api = new api();
-            let URL=Api.requete_url("now_playing");
-            fetch(URL)
-            .then(res=>res.json())
-            .then((data)=>{
-                data=data.results;
-                console.log(data);
-                this.setState({donnée:data});
-                console.log("state",this.state.donnée[1].title)
-            })
-        }
-    }  
-    
     componentDidMount(){
         let Api = new api();
         let URL=Api.requete_url("now_playing");
-        fetch(URL)
-        .then(res=>res.json())
-        .then((data)=>{
-            data=data.results;
-            console.log(data);
+        (async () => {
+            let response = await fetch(URL);
+            let data = await response.json();
+            data = await data.results;
             this.setState({donnée:data});
-            console.log("state",this.state.donnée[1].title)
-            this.render()
-        })
+        })();
     }
 
     render() {  
-        return (
-            <div className="container-fluid" id="accpage">
-                <div></div>                
-                <table>
-                    <tbody>
-                        <tr>
-                            
-                            <td className="preview"><Preview titre={this.state.donnée[1].title} affiche="https://m.media-amazon.com/images/I/8129a7-9A7L._AC_SX679_.jpg"/></td>
-                            {/*<td className="preview"><Preview numero={2} request = "now_playing"/></td>
-                            <td className="preview"><Preview numero={3} request = "now_playing"/></td>
-                            <td className="preview"><Preview numero={4} request = "now_playing"/></td>
-                            
-                        </tr>
-                        <tr>
-                            <td className="preview"><Preview numero={5} request = "now_playing"/></td>
-                            <td className="preview"><Preview numero={6} request = "now_playing"/></td>
-                            <td className="preview"><Preview numero={7} request = "now_playing"/></td>
-            <td className="preview"><Preview numero={8} request = "now_playing"/></td>*/}
-                            
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
+        if (this.state.donnée==null){
+            return <div className="container-fluid" id="accpage"></div> ;
+        }else{
+            return (
+                
+                <div className="container-fluid" id="accpage">
+                    <div></div>                
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className="preview"><Preview titre={this.state.donnée[0].title} affiche="https://m.media-amazon.com/images/I/8129a7-9A7L._AC_SX679_.jpg"/></td>
+                                <td className="preview"><Preview titre={this.state.donnée[1].title} affiche=""/></td>
+                                <td className="preview"><Preview titre={this.state.donnée[2].title} affiche=""/></td>
+                                <td className="preview"><Preview titre={this.state.donnée[3].title} affiche=""/></td>
+                                
+                            </tr>
+                            <tr>
+                                <td className="preview"><Preview titre={this.state.donnée[4].title} affiche=""/></td>
+                                <td className="preview"><Preview titre={this.state.donnée[5].title} affiche=""/></td>
+                                <td className="preview"><Preview titre={this.state.donnée[6].title} affiche=""/></td>
+                            <td className="preview"><Preview titre={this.state.donnée[7].title} affiche=""/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
     }
 }
